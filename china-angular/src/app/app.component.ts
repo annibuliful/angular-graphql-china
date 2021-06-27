8b0d40459bed804454dd0785693139355ebe0fe9
lagmanzaza <lagmanmc@gmail.com>
2021-06-27 23:25:24 +0700

    feat: complete

--- china-angular/src/app/app.component.ts
+++ china-angular/src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
 
+const GET_BOOKS = gql`
+  query getAllBooks {
+    books {
+      title
+      author
+    }
+  }
+`;
+
+interface IBook {
+  title: string;
+  author: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'china-angular';
+  listBooks: IBook[] = [];
+  loading = true;
+
+  constructor(apollo: Apollo) {
+    apollo
+      .watchQuery<{ books: IBook[] }>({
+        query: GET_BOOKS,
+      })
+      .valueChanges.subscribe(({ data, loading }) => {
+        this.listBooks = data.books;
+        this.loading = loading;
+      });
+  }
 }
