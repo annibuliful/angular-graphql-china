772599c449d1fe24ff8ece652594110dc4a7b0b7
lagmanzaza <lagmanmc@gmail.com>
2021-06-28 22:17:19 +0700

    feat: subscription

--- china-angular/src/app/app.component.ts
+++ china-angular/src/app/app.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
-import { Apollo, gql } from 'apollo-angular';
+import { Observable } from '@apollo/client/utilities';
+import { Apollo, gql, QueryRef } from 'apollo-angular';
 
 const GET_BOOKS = gql`
   query getAllBooks {
     books {
+      title
+      author
+    }
+  }
+`;
+
+const SUBSCRIBE_CREATE_BOOK = gql`
+  subscription subscribCreateBook($isEven: Boolean) {
+    bookCreatedWithFilter(isEven: $isEven) {
+      id
       title
       author
     }
@@ -23,15 +34,34 @@ export class AppComponent {
   title = 'china-angular';
   listBooks: IBook[] = [];
   loading = true;
+  books: Observable<any>;
+  getBookQuery: QueryRef<any>;
 
   constructor(apollo: Apollo) {
-    apollo
-      .watchQuery<{ books: IBook[] }>({
-        query: GET_BOOKS,
-      })
-      .valueChanges.subscribe(({ data, loading }) => {
-        this.listBooks = data.books;
-        this.loading = loading;
-      });
+    this.getBookQuery = apollo.watchQuery<{ books: IBook[] }>({
+      query: GET_BOOKS,
+    });
+
+    this.books = this.getBookQuery.valueChanges as any;
+
+    this.books.subscribe(({ data, loading }) => {
+      this.listBooks = data.books;
+      this.loading = loading;
+    });
+
+    this.getBookQuery.subscribeToMore({
+      document: SUBSCRIBE_CREATE_BOOK,
+      variables: {
+        isEven: false,
+      },
+      updateQuery: (prev, { subscriptionData }) => {
+        console.log('prev', prev);
+        console.log('subscriptionData', subscriptionData.data);
+        return {
+          ...prev,
+          books: [...prev.books, subscriptionData.data.bookCreatedWithFilter],
+        };
+      },
+    });
   }
 }
--- china-angular/src/app/graphql.module.ts
+++ china-angular/src/app/graphql.module.ts
@@ -1,12 +1,42 @@
 import { NgModule } from '@angular/core';
 import { APOLLO_OPTIONS } from 'apollo-angular';
-import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import { ApolloClientOptions, InMemoryCache, split } from '@apollo/client/core';
+import { WebSocketLink } from '@apollo/client/link/ws';
 import { HttpLink } from 'apollo-angular/http';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { OperationDefinitionNode } from 'graphql';
 
 const uri = 'http://localhost:4000/'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+  // Create an http link:
+  const http = httpLink.create({
+    uri: 'http://localhost:4000/graphql',
+  });
+
+  // Create a WebSocket link:
+  const ws = new WebSocketLink({
+    uri: `ws://localhost:4000/graphql`,
+    options: {
+      reconnect: true,
+    },
+  });
+
+  // using the ability to split links, you can send data to each link
+  // depending on what kind of operation is being sent
+  const link = split(
+    // split based on operation type
+    ({ query }) => {
+      const { kind, operation } = getMainDefinition(
+        query
+      ) as OperationDefinitionNode;
+      return kind === 'OperationDefinition' && operation === 'subscription';
+    },
+    ws,
+    http
+  );
+
   return {
-    link: httpLink.create({ uri }),
+    link,
     cache: new InMemoryCache(),
   };
 }
